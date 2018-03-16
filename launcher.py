96b9f3edd70e17548033f0471dfaf485055ebb04
animalize <malincns@163.com>
2018-03-16 17:09:45 +0800

    检查新版本 2

--- launcher.py
+++ launcher.py
@@ -14,11 +14,12 @@ TING_VERSION = 1
     
 def main():
     p = Process(target = web_server.server_main, 
-                args = (PORT, FULL_VERSION, True),
-                daemon=True)
+                args = (PORT,),
+                daemon = True)
     p.start()
     
-    pc_side.pc_main('http://127.0.0.1:' + str(PORT))
+    pc_side.pc_main('http://127.0.0.1:' + str(PORT),
+                    FULL_VERSION, True)
     
 if __name__ == '__main__':
     main()
